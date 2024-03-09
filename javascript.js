02d2be2ba733a9d4b15e6b5679ae1b50114d03b9
kamal joshi <kamaljoshi673243@gmail.com>
2024-03-10 00:48:50 +0530

    my portfolio website

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// const checkbox = document.getElementById("checkbox")
+// checkbox.addEventListener("change", () => {
+//   document.body.classList.toggle("dark")
+// })
+
+function speak() {
+  let synth = window.speechSynthesis;
+  let textPara = document.getElementById('textbtn');
+  let text = textPara.innerText;
+  let utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+}
