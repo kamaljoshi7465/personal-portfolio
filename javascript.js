adb36cf38fd043194b7d3eac5a988d8c5235667d
kamal joshi <kamaljoshi673243@gmail.com>
2024-03-10 23:19:10 +0530

    added js to dark/light mode

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-// const checkbox = document.getElementById("checkbox")
-// checkbox.addEventListener("change", () => {
-//   document.body.classList.toggle("dark")
-// })
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
 
 function speak() {
   let synth = window.speechSynthesis;
@@ -10,3 +10,24 @@ function speak() {
   let utterance = new SpeechSynthesisUtterance(text);
   synth.speak(utterance);
 }
+
+//   var currentIndex = 0;
+//   var projects = document.getElementById('projects');
+//   var projectItems = document.getElementsByClassName('project');
+//   var totalItems = projectItems.length;
+//   var projectWidth = projectItems[0].offsetWidth; // Assuming all project items have the same width
+
+//   document.getElementById('arrow-right').addEventListener('click', function () {
+//     currentIndex = (currentIndex - 1) % totalItems;
+//     updateSlider();
+//   });
+
+//   document.getElementById('arrow-left').addEventListener('click', function () {
+//     currentIndex = (currentIndex + 1) % totalItems;
+//     updateSlider();
+//   });
+
+//   function updateSlider() {
+//     projects.style.transform = 'translateX(' + (-projectWidth * currentIndex) + 'px)';
+//   }
+// });
