d7d54cf3cba6b2816eb4da156f206b0fd96a7efc
kamal joshi <kamaljoshi673243@gmail.com>
2024-03-10 23:20:55 +0530

    added js to dark/light mode

--- javascript.js
+++ javascript.js
@@ -10,24 +10,3 @@ function speak() {
   let utterance = new SpeechSynthesisUtterance(text);
   synth.speak(utterance);
 }
-
-//   var currentIndex = 0;
-//   var projects = document.getElementById('projects');
-//   var projectItems = document.getElementsByClassName('project');
-//   var totalItems = projectItems.length;
-//   var projectWidth = projectItems[0].offsetWidth; // Assuming all project items have the same width
-
-//   document.getElementById('arrow-right').addEventListener('click', function () {
-//     currentIndex = (currentIndex - 1) % totalItems;
-//     updateSlider();
-//   });
-
-//   document.getElementById('arrow-left').addEventListener('click', function () {
-//     currentIndex = (currentIndex + 1) % totalItems;
-//     updateSlider();
-//   });
-
-//   function updateSlider() {
-//     projects.style.transform = 'translateX(' + (-projectWidth * currentIndex) + 'px)';
-//   }
-// });
