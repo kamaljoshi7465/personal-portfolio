e65f20c51f412a1fcddd827fd75a1345f21b5e51
kamal joshi <kamaljoshi673243@gmail.com>
2024-03-17 23:06:57 +0530

    home page, search bar

--- javascript.js
+++ javascript.js
@@ -10,3 +10,43 @@ function speak() {
   let utterance = new SpeechSynthesisUtterance(text);
   synth.speak(utterance);
 }
+
+
+function searchAndScroll() {
+  const searchQuery = document.getElementById('searchInput').value.toLowerCase();
+  const section = document.getElementById(searchQuery);
+
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  } else {
+    alert('Section not found');
+  }
+}
+
+const sections = [
+  { id: 'home', name: 'Home' },
+  { id: 'about', name: 'About Me' },
+  { id: 'projects', name: 'Projects' },
+  { id: 'gallery', name: 'My Images' },
+  { id: 'skills', name: 'Skills' },
+  { id: 'contact-us', name: 'Contact me' },
+  // Add more sections as needed
+];
+
+function showSuggestions(input) {
+  const suggestionsContainer = document.getElementById('suggestionsContainer');
+  suggestionsContainer.innerHTML = '';
+  if (input.length > 0) {
+    const filteredSections = sections.filter(section => section.name.toLowerCase().includes(input.toLowerCase()));
+    filteredSections.forEach(section => {
+      const div = document.createElement('div');
+      div.innerText = section.name;
+      div.onclick = function () {
+        document.getElementById('searchInput').value = section.name;
+        suggestionsContainer.innerHTML = '';
+        document.getElementById(section.id).scrollIntoView({ behavior: 'smooth', block: 'start' });
+      };
+      suggestionsContainer.appendChild(div);
+    });
+  }
+}
